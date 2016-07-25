567d2c6ac352d8c22545ea6e28401bc96139a631
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-07-25 22:34:31 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class num {
+
+	public static void main(String[] args) {
+		for(;;){
+	Scanner i_p=new Scanner(System.in);
+	int num=i_p.nextInt();
+	if((num%2)==0)
+		System.out.println("even");
+	else
+		System.out.println("odd");
+		
+
+	}
+	}
+}
