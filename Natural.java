3f803c69211f676d42c08a76d0ca9443def0c88f
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-08-01 19:25:00 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,23 @@
+package alpha;
+
+import java.util.Scanner;
+
+/*To Find Sum of natural nums
+ * created by sanjayKumar on 01-08-16
+ */
+public class Natural{
+	
+
+	public static void main(String[] args) { 
+		for(;;){
+Scanner Sc=new Scanner(System.in);
+	int num=1,a,b=0;
+	a=Sc.nextInt();
+	for(;num<=a;num++){
+		b+=num;
+		
+	}
+	System.out.println("sum of natural num is"+" "+b);
+			}
+		}}
+
