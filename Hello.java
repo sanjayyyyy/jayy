0af0e26742e9bf2c4fe9d99026003502f48158c5
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-08-01 09:14:37 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package alpha;
+/*To Print Hello Word 5 times
+ * created by sanjayKumar on 01-08-16
+ */
+public class Hello{
+
+	public static void main(String[] args)  {
+	int Sc=1;
+		while(Sc<=5){
+		System.out.println("Hello");
+		Sc++;
+			}
+		}
+}
