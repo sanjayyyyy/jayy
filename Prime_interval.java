2e94c46ccc8482f01b31210368630c31ae7c5b45
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-11-02 23:20:51 +0530

    Create  Prime_interval.java

---
+++ Prime_interval.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Prime_interval {
+	
+	
+   public static void main(String[] args) {
+	Scanner Sc=new Scanner(System.in);
+	int x=Sc.nextInt(),y=Sc.nextInt();
+
+	for(int j=x;j<=y;j++){
+	for(int i=2;i<=y;i++){
+		int temp=j%i;
+		if(temp==0&&i==j&&j%2!=0){
+			
+			System.out.println("prime nums are"+j);}
+			
+		
+		
+		
+	
+		
+		
+		
+		
+		
+	}
+	}
+			
+		}
+		
+			
+
+
+	}
+
+
