658d82cf19c04309595e79f9316786245563b533
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-11-02 22:05:48 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class odd {
+	
+	
+   public static void main(String[] args) {
+	   Scanner Sc=new Scanner(System.in);
+	   int x=Sc.nextInt(),y=Sc.nextInt();
+	   for(int i=x;i<=y;i++){
+		   if(i%2!=0)
+			   System.out.println("odd numbers are"+" "+i);
+		   
+		   
+	   }
+	  
+
+	
+	
+			
+			
+		}
+		
+			
+
+
+	}
+
+
