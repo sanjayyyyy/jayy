5d39650465d61cdba5fd09a7997a205010e01657
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-11-02 22:07:48 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class even {
+	
+	
+   public static void main(String[] args) {
+	   Scanner Sc=new Scanner(System.in);
+	   int x=Sc.nextInt(),y=Sc.nextInt();
+	   for(int i=x;i<=y;i++){
+		   if(i%2==0)
+			   System.out.println("even numbers are"+" "+i);
+		   
+		   
+	   }
+	  
+
+	
+	
+			
+			
+		}
+		
+			
+
+
+	}
+
+
