ceef8081cedb791c2436b6a5f7ef919ecafcb6ff
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-11-04 20:24:47 +0530

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,28 @@
+/* Multiplication tables
+ * created by SanjayKumar On 4/11/16
+ * */
+import java.util.*;
+public class Table {
+	
+	
+	
+   public static void main(String[] args) {
+	   Scanner Sc=new Scanner(System.in);
+	   int num=Sc.nextInt(),Start=Sc.nextInt(),end=Sc.nextInt();
+	   for(int i=Start;i<=end;i++){
+		   System.out.println(num+"*"+i+"="+" "+num*i);
+		   
+		   
+	   }
+	  
+	 
+}
+
+
+	
+		}
+		
+			
+
+
+
