8caf2ab2f166a01093233994873ad6ff85cdbddf
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-11-05 21:12:38 +0530

    Create Exit.java

---
+++ Exit.java
@@ -0,0 +1,30 @@
+/*exit code 
+ * created by SanjayKumar On 5/11/16
+ * */
+import java.io.*;
+
+public class Exit {
+	
+	
+	
+	
+   public static void main(String[] args) throws java.lang.Exception{
+	
+	  BufferedReader Br=new BufferedReader(new InputStreamReader(System.in));
+	  String s=Br.readLine();
+	
+	if(s.equals("q"))
+		System.exit(0);
+	else System.out.println("no exit");
+	  
+	 
+}
+
+
+	
+		}
+		
+			
+
+
+
