68d8f0697d68737b5a61fa2e6ff0a83c8a03d872
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-11-05 22:41:08 +0530

    Update and rename Sum.java to Sum_all.java

---
+++ Sum_all.java
@@ -0,0 +1,31 @@
+/*addition of all numbers 
+ * created by SanjayKumar On 5/11/16
+ * */
+import java.io.*;
+
+public class Sum_all {
+	
+	
+	
+	
+   public static void main(String[] args) throws java.lang.Exception{
+	
+	  BufferedReader Br=new BufferedReader(new InputStreamReader(System.in));
+	  int num=Integer.parseInt(Br.readLine()),temp=0;
+	  for(int i=1;i<=num;i++){
+		   temp+=i;
+		  
+	  }
+	  System.out.println(temp);
+	
+	 
+}
+
+
+	
+		}
+		
+			
+
+
+
