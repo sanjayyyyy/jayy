a7d59bf0795661ab91b82849d1c3fa02831b74be
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-11-05 18:29:41 +0530

    Create Ascii.java

---
+++ Ascii.java
@@ -0,0 +1,33 @@
+/*Ascii values
+ * created by SanjayKumar On 5/11/16
+ * */
+import java.util.*;
+public class Ascii {
+	
+	
+	
+	
+   public static void main(String[] args) {
+	String s="";
+	
+	for(int i=1;i<=255;i++){
+		s=new Character((char)i).toString();
+		System.out.println(i+" "+"-->"+" "+s);
+		
+	}
+	   
+	   
+	 
+	 
+	  
+	 
+}
+
+
+	
+		}
+		
+			
+
+
+
