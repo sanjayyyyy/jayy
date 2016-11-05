e501a525006b744276f715301f8d8dd7d97052c0
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-11-05 22:57:18 +0530

    Create Odd_sum.java

---
+++ Odd_sum.java
@@ -0,0 +1,37 @@
+/*addition of numbers and odd numbers 
+ * created by SanjayKumar On 5/11/16
+ * */
+import java.io.*;
+
+public class Odd_sum.java {
+	
+	
+	
+	
+   public static void main(String[] args) throws java.lang.Exception{
+	
+	  BufferedReader Br=new BufferedReader(new InputStreamReader(System.in));
+	  int temp=0,temp1=0;
+	  for(int i=1;i<=45;i++){
+		  if(i<=15)
+		  temp+=i;
+		
+		   if(i>=15&&i%2!=0)
+			  temp1+=i;
+			  
+			  
+		  
+	  }
+	  System.out.println(temp);
+	  System.out.println(temp1);
+	
+	  
+
+
+   }
+		}
+		
+			
+
+
+
