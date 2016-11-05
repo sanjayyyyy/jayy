2e1eec9d212c8ef84d25558aeb601ec20f9eec10
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-11-05 18:03:34 +0530

    Create Gcd.java

---
+++ Gcd.java
@@ -0,0 +1,37 @@
+/*HCF or Gcd
+ * created by SanjayKumar On 5/11/16
+ * */
+import java.util.*;
+public class Gcd {
+	
+	
+	
+	
+   public static void main(String[] args) {
+	   Scanner Sc=new Scanner(System.in);
+	   int num=Sc.nextInt();
+	   
+	   for(int i=num;i>=1;i--){
+		   if(num%i==0){
+			   System.out.println(i);
+			   break;
+		   }
+		   
+	   }
+	   
+	   
+	   
+	 
+	 
+	  
+	 
+}
+
+
+	
+		}
+		
+			
+
+
+
