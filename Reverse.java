8b2b538ddeef5d69128dea457300c47259eb4962
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-11-05 18:23:28 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,31 @@
+/*integer reverse
+ * created by SanjayKumar On 5/11/16
+ * */
+import java.util.*;
+public class Reverse {
+	
+	
+	
+	
+   public static void main(String[] args) {
+	  
+	  String S=new StringBuffer(new Scanner(System.in).next()).reverse().toString();
+	  int reverse=Integer.parseInt(S);
+	  System.out.println(reverse);
+	   
+	   
+	   
+	 
+	 
+	  
+	 
+}
+
+
+	
+		}
+		
+			
+
+
+
