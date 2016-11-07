f22cc34c052ac15db72ab772ed77cffca5efcaa3
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-11-07 13:56:01 +0530

    Create Count_words.java

---
+++ Count_words.java
@@ -0,0 +1,36 @@
+/*Count words 
+ * created by SanjayKumar On 6/11/16
+ * */
+import java.io.*;
+public class Count_words {
+	
+	
+	
+	
+   public static void main(String[] args) throws Exception {
+	   BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	   String Line=br.readLine();
+	   int count=0;
+	   
+	   for(int i=0;i<Line.length()-1;i++){
+		   if(((Line.charAt(i)==' ')||(i==0))&&(Line.charAt(i+1)!=' '))
+			   count++;
+			   
+		   
+		   
+	   }
+	   System.out.println(count);
+
+		   }
+		   
+	   }
+	   
+	   
+	   
+	 
+	 
+	  
+
+		
+			
+
