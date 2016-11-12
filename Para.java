0f05ddacdb3b4b510c8e2b406d96853d54668970
sanjayyyyy <sanjayyyyy@users.noreply.github.com>
2016-11-12 10:52:20 +0530

    Create Para.java

---
+++ Para.java
@@ -0,0 +1,33 @@
+/*Count lines in a Para 
+ * created by SanjayKumar On 12/11/16
+ * */
+import java.util.*;
+public class Para {
+	
+	
+	
+	
+   public static void main(String[] args){
+	   Scanner Sc=new Scanner(System.in);
+	   String Line=Sc.nextLine();
+	   
+	
+	  
+	   System.out.println(Line.split("[\n]").length);
+	   
+	   
+	  
+
+		   }
+		   
+	   }
+	   
+	   
+	   
+	 
+	 
+	  
+
+		
+			
+
